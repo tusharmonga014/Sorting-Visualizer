8ebe701d798cfbe9bae317ef0710388a2238c7bf
tusharmonga014 <tusharmongatt@gmail.com>
2021-07-24 02:20:55 +0530

    Updated control bar slide controls

--- src/components/ControlBar/ControlBar.jsx
+++ src/components/ControlBar/ControlBar.jsx
@@ -144,22 +144,22 @@ class ControlBar extends Component {
         </div>
 
         <div className='col-md-12 col-lg-3 row slider-controls'>
-          <div className='text-white text-center text-disable-when-running slider-controls col-md-6 col-lg-6'>
+          <div className='text-white text-center text-disable-when-running slider-controls col-sm-3 col-md-6 col-lg-6'>
             Array Size :
           </div>
           <input
-            className='input-disable-when-running slider-controls col-md-6 col-lg-6'
+            className='input-disable-when-running text-center slider-controls slider-range col-sm-7 col-md-6 col-lg-6'
             type="range"
             min={MIN_ARRAY_SIZE}
             max={MAX_ARRAY_SIZE}
             defaultValue={DEFAULT_ARRAY_SIZE}
             onChange={this.changeArraySize}
           />
-          <div className='text-white text-center text-disable-when-running slider-controls col-md-6 col-lg-6'>
+          <div className='text-white text-center text-disable-when-running slider-controls col-sm-3 col-md-6 col-lg-6'>
             Sorting Speed :
           </div>
           <input
-            className='input-disable-when-running slider-controls col-md-6 col-lg-6'
+            className='input-disable-when-running text-center slider-controls slider-range col-sm-7 col-md-6 col-lg-6'
             type="range"
             min="1"
             max={SORTING_SPEED_UPPER_LIMIT}
